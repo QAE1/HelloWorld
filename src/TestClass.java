e5fc49f6890689222cf71e8c91958208b323c41f
Artur <tkachev.a.05@mail.ru>
2025-10-01 18:24:44 +0700

    тестовый коммит2

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class TestClass {
     public static void main(String[] args) {
     System.out.println("Результат вычислений "+calc(5));
-
+    System.out.println("Результат вычислений "+calc(5));
     }
 
     public static int calc(int a) {
